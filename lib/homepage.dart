24ebc6c23f8e77469a00df79532f43d046392e85
Nathaleon <nathaleonrdk@gmail.com>
2025-03-10 16:33:40 +0700

    Push

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'trapesiumpage.dart';
 import 'kubuspage.dart';
 import 'haripage.dart';
+import 'datadiri.dart'; // Import halaman DataDiri
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,100 +11,93 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-            child: Text(
-          "KUIS TEKNOLOGI PEMROGRAMAN MOBILE",
-          style: TextStyle(fontWeight: FontWeight.w900),
-        )),
+        title: const Center(
+          child: Text(
+            "KUIS TEKNOLOGI PEMROGRAMAN MOBILE",
+            style: TextStyle(fontWeight: FontWeight.w900),
+          ),
+        ),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
         child: Column(
           children: [
+            // Tombol untuk menuju halaman Data Diri
             Container(
-                padding: EdgeInsets.only(bottom: 10),
-                width: double.infinity,
-                child: Center(
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset('assets/images/Nathaleon.jpg')),
-                )),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color.fromARGB(255, 235, 221, 203),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
               width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Nama   : Nathaleon Ranggainaya Dian Kuncoro",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "NIM    : 123220015",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Kelas  : IF-B",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Hobi : Bermain Gitar",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  )
-                ],
+              constraints: const BoxConstraints(minHeight: 80),
+              padding: const EdgeInsets.only(bottom: 10),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const DataDiriPage()),
+                  );
+                },
+                child: const Text(
+                  "DATA DIRI",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
+
+            // Tombol Trapesium
             Container(
               width: double.infinity,
-              constraints: BoxConstraints(minHeight: 80),
-              padding: EdgeInsets.only(bottom: 10),
+              constraints: const BoxConstraints(minHeight: 80),
+              padding: const EdgeInsets.only(bottom: 10),
               child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => TrapesiumPage()));
-                  },
-                  child: Text(
-                    "TRAPESIUM",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const TrapesiumPage()),
+                  );
+                },
+                child: const Text(
+                  "TRAPESIUM",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
+
+            // Tombol Kubus
             Container(
               width: double.infinity,
-              constraints: BoxConstraints(minHeight: 80),
-              padding: EdgeInsets.only(bottom: 10),
+              constraints: const BoxConstraints(minHeight: 80),
+              padding: const EdgeInsets.only(bottom: 10),
               child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => KubusPage()));
-                  },
-                  child: Text("KUBUS",
-                      style: TextStyle(fontWeight: FontWeight.bold))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const KubusPage()),
+                  );
+                },
+                child: const Text(
+                  "KUBUS",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
+
+            // Tombol Hitung Hari
             Container(
               width: double.infinity,
-              constraints: BoxConstraints(minHeight: 80),
-              padding: EdgeInsets.only(bottom: 10),
+              constraints: const BoxConstraints(minHeight: 80),
+              padding: const EdgeInsets.only(bottom: 10),
               child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => HariPage()));
-                  },
-                  child: Text("HITUNG HARI",
-                      style: TextStyle(fontWeight: FontWeight.bold))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HariPage()),
+                  );
+                },
+                child: const Text(
+                  "HITUNG HARI",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
           ],
         ),
